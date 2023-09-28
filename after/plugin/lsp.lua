db8c7bca961ec333e06ea4fc0d346878aa7e9b87
vague2k <121782036+vague2k@users.noreply.github.com>
2023-09-28 01:06:23 -0400

    Using lsp-zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,50 +1,53 @@
-local lspconfig = require('lspconfig')
-local cmp_nvim_lsp = require('cmp_nvim_lsp')
-local capabilities = cmp_nvim_lsp.default_capabilities()
+local lsp_zero = require("lsp-zero")
 
-local on_attach = function(_, bufnr)
+lsp_zero.on_attach(
 
-    local bufmap = function(keys, func)
-        vim.keymap.set('n', keys, func, { buffer = bufnr })
+    function (_, bufnr)
+
+        local bufmap = function(keys, func)
+            vim.keymap.set('n', keys, func, { buffer = bufnr })
+        end
+
+        bufmap('<leader>r', vim.lsp.buf.rename)
+        bufmap('<leader>a', vim.lsp.buf.code_action)
+
+        bufmap('gd', vim.lsp.buf.definition)
+        bufmap('gD', vim.lsp.buf.declaration)
+        bufmap('gI', vim.lsp.buf.implementation)
+        bufmap('T', vim.lsp.buf.type_definition)
+        bufmap('K', vim.lsp.buf.hover)
+
+        bufmap('gr', require('telescope.builtin').lsp_references)
+        bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+        bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+
+        bufmap('<leader>dl', ':Telescope diagnostics bufnr=0<CR>')
+        bufmap('<leader>d', vim.diagnostic.open_float)
+        bufmap('<leader>dn', vim.diagnostic.goto_next)
+        bufmap('<leader>dp', vim.diagnostic.goto_prev)
+
     end
 
-    bufmap('<leader>r', vim.lsp.buf.rename)
-    bufmap('<leader>a', vim.lsp.buf.code_action)
+)
 
-    bufmap('gd', vim.lsp.buf.definition)
-    bufmap('gD', vim.lsp.buf.declaration)
-    bufmap('gI', vim.lsp.buf.implementation)
-    bufmap('T', vim.lsp.buf.type_definition)
-    bufmap('K', vim.lsp.buf.hover)
+local function organize_imports()
+    local params = {
+        command = '_typescript.organizeImports',
+        arguments = { vim.api.nvim_buf_get_name(0) }
+    }
+    vim.lsp.buf.execute_command(params)
 
-    bufmap('gr', require('telescope.builtin').lsp_references)
-    bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-    bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
-
-    bufmap('<leader>dl', ':Telescope diagnostics bufnr=0<CR>')
-    bufmap('<leader>d', vim.diagnostic.open_float)
-    bufmap('<leader>dn', vim.diagnostic.goto_next)
-    bufmap('<leader>dp', vim.diagnostic.goto_prev)
-
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, {})
 end
 
--- mason setup --
-local default_setup = function(server)
-    lspconfig[server].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-    })
-end
+lsp_zero.setup()
 
-require("mason").setup()
+require('mason').setup({})
 require('mason-lspconfig').setup({
 
     ensure_installed = {
         'lua_ls',
         'tsserver',
+        'biome',
         'astro',
         'gopls',
         'pyright',
@@ -53,13 +56,11 @@ require('mason-lspconfig').setup({
     },
 
     handlers = {
-        default_setup,
+        lsp_zero.default_setup,
 
         lua_ls = function()
             require('neodev').setup()
             require('lspconfig').lua_ls.setup {
-                on_attach = on_attach,
-                capabilities = capabilities,
                 settings = {
                     Lua = {
                         diagnostics = {
@@ -71,5 +72,22 @@ require('mason-lspconfig').setup({
                 }
             }
         end,
-    }
+
+
+        tsserver = function()
+            require('lspconfig').tsserver.setup {
+                init_options = {
+                    preferences = {
+                        disableSuggestions = true,
+
+                    }
+                },
+                command = {
+                    OrganizeImports = {
+                        organize_imports
+                    }
+                }
+            }
+        end,
+    },
 })
