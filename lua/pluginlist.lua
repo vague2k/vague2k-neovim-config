79358825c4378a9dca0d65ebeb5b11dc1bf1e314
vague2k <ilovedrawing056@gmail.com>
2023-11-17 15:58:53 -0500

    Added conform.nvim (formatter plugin)

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -1,79 +1,114 @@
 return {
     {
         ------- Theme config --
-        'rebelot/kanagawa.nvim',
+        "deparr/tairiki.nvim",
+        lazy = false,
         priority = 1000,
         config = function()
-            -- setup must be called before loading the colorscheme
-            -- These are all the default options set to my own tastes:
-            require('kanagawa').setup({
-                keywordStyle = { italic = false },
-                statementStyle = { bold = false },
-                colors = {
-                    theme = { dragon = { ui = { float = { 'none' } } }},
+            require("tairiki").setup({
+                -- optional configuration here
+            })
+            require("tairiki").load()
+        end,
+        -- priority = 1000,
+        -- config = function()
+        --     -- setup must be called before loading the colorscheme
+        --     -- These are all the default options set to my own tastes:
+        --     require('kanagawa').setup({
+        --         keywordStyle = { italic = false },
+        --         statementStyle = { bold = false },
+        --         colors = {
+        --             theme = { dragon = { ui = { float = { 'none' } } }},
+        --         },
+        --     })
+        --     vim.cmd([[ colorscheme kanagawa-dragon ]])
+        --
+        --     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        --     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+        -- end
+    },
+
+    ------- LSP, auto cmp, formatting, and treesitter syntax highlighting --
+    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "neovim/nvim-lspconfig" },
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim" },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "hrsh7th/cmp-nvim-lua",
+            "lukas-reineke/cmp-rg",
+        },
+    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local conform = require("conform")
+
+            conform.setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    python = { "isort", "black" },
+                    javascript = { "prettier" },
+                    typecript = { "prettier" },
+                    javascriptreact = { "prettier" },
+                    typescriptreact = { "prettier" },
+                    css = { "prettier" },
+                    html = { "prettier" },
+                    markdown = { "prettier" },
+                },
+
+                format_on_save = {
+                    lsp_fallback = true,
+                    async = false,
+                    timeout_ms = 500,
                 },
             })
-            vim.cmd([[ colorscheme kanagawa-dragon ]])
-
-            vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-            vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-        end
+        end,
     },
 
-------- LSP, auto cmp, and treesitter syntax highlighting --
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    {'neovim/nvim-lspconfig'},
-    {'antosha417/nvim-lsp-file-operations', config = true},
-    {'folke/neodev.nvim'},
+    ------- Telescope for easy file search & fzf --
     {
-        'hrsh7th/nvim-cmp',
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
         dependencies = {
-            'hrsh7th/cmp-nvim-lsp',
-            'L3MON4D3/LuaSnip',
-            'hrsh7th/cmp-nvim-lua',
-            'lukas-reineke/cmp-rg',
-
-        },
-    },
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate', },
-
-------- Telescope for easy file search & fzf --
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-tree/nvim-web-devicons',
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
         },
     },
 
-------- Dressing.nvim for better ui for vim.ui.input/.select
-    { 'stevearc/dressing.nvim', },
+    ------- Dressing.nvim for better ui for vim.ui.input/.select
+    { "stevearc/dressing.nvim" },
 
-------- For easy commenting of lines and such --
+    ------- For easy commenting of lines and such --
     {
-        'numToStr/Comment.nvim',
+        "numToStr/Comment.nvim",
         config = function()
-            require('Comment').setup()
-        end
+            require("Comment").setup()
+        end,
     },
 
-------- My status line of choice --
+    ------- My status line of choice --
     {
-        'nvim-lualine/lualine.nvim',
+        "nvim-lualine/lualine.nvim",
         dependecies = {
-            'nvim-tree/nvim-web-devicons'
+            "nvim-tree/nvim-web-devicons",
         },
         config = function()
-            require('lualine').setup({
+            require("lualine").setup({
                 icons_enabled = true,
-                theme = 'aurora',
+                theme = "aurora",
             })
-        end
+        end,
     },
 
-------- File tree -- 
+    ------- File tree --
     {
         "nvim-tree/nvim-tree.lua",
         version = "*",
@@ -83,23 +118,23 @@ return {
         },
     },
 
-------- Welcome screen for easy access to frequented files/dirs --
+    ------- Welcome screen for easy access to frequented files/dirs --
     {
-        'goolord/alpha-nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function ()
-            require'alpha'.setup(require'alpha.themes.startify'.config)
-        end
+        "goolord/alpha-nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("alpha").setup(require("alpha.themes.startify").config)
+        end,
     },
 
-------- Terminal emulator -- 
-    { 'akinsho/toggleterm.nvim', version = "*", config = true },
+    ------- Terminal emulator --
+    { "akinsho/toggleterm.nvim", version = "*", config = true },
 
-------- Auto cmp on (), [], {} and html tags! -- 
+    ------- Auto cmp on (), [], {} and html tags! --
     {
-        'windwp/nvim-autopairs',
+        "windwp/nvim-autopairs",
         event = "InsertEnter",
-        opts = {} -- this is equalent to setup({}) function
+        opts = {}, -- this is equalent to setup({}) function
     },
-    { 'windwp/nvim-ts-autotag' }
+    { "windwp/nvim-ts-autotag" },
 }
