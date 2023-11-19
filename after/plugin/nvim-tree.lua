b903093380a31600d4c2931c9b4e79ed4068780b
vague2k <121782036+vague2k@users.noreply.github.com>
2023-11-19 06:59:07 -0500

    Update nvim-tree.lua

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,11 +4,14 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    git = {
+        ignore = false,
+    },
 })
