044083e05434527128d915e43d03844893cb5aa3
vague2k <ilovedrawing056@gmail.com>
2023-12-04 12:07:31 -0500

    Never noticed i didn't have friendly snippets lol

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -55,11 +55,9 @@ cmp.setup({
             item.kind = (icons[item.kind] or "") .. item.kind
             item.menu = ({
                 nvim_lsp = "[LSP]",
-                buffer = "[Buffer]",
-                luasnip = "[Snippet]",
-                nvim_lua = "[API]",
-                path = "[Path]",
-                rg = "[RG]",
+                buffer = "[BUF]",
+                luasnip = "[SNIP]",
+                path = "[PATH]",
             })[entry.source.name]
             return item
         end,
@@ -80,31 +78,30 @@ cmp.setup({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
     }),
 
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
-        { name = "nvim_lua" },
         { name = "buffer" },
         { name = "path" },
+    }),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" },
+    },
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        { name = "cmdline" },
     }),
 })
--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -19,9 +19,13 @@ return {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
             "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
             "hrsh7th/cmp-nvim-lua",
-            "lukas-reineke/cmp-rg",
+            "rafamadriz/friendly-snippets",
         },
     },
 
