55afd584a346673ce920bed05fa7bc6351f54166
vague2k <ilovedrawing056@gmail.com>
2023-12-22 19:00:01 -0500

    Update autopairs.lua

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -1,3 +1,25 @@
+local npairs = require("nvim-autopairs")
+
+-- default setup ripped straight off github, put here for documentation purposes
+-- setup subject to change according to my taste
+
+-- change default fast_wrap
+npairs.setup({
+    fast_wrap = {
+        map = "<A-w>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = "$",
+        before_key = "h",
+        after_key = "l",
+        cursor_pos_before = true,
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        manual_position = true,
+        highlight = "Search",
+        highlight_grey = "Comment",
+    },
+})
+
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
